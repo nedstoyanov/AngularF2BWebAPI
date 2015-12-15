990e01212f6055d3403f59e30b36119ee80fb39c
nedstoyanov <ned_77@iinet.net.au>
2015-12-15 21:49:21 +1000

    initial angular web api project

---
+++ APM.Client/common/productResource.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("productResource",
+                 ["$resource", "appSettings", productReource])
+
+    function productResource($resource, appSettings) 
+    {
+        return $resource(appSettings.serverPAth + "/api/products/:id");
+    }
+
+}());
